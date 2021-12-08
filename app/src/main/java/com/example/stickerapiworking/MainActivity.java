51c9fbd0be91913baf64e5e6c33a6083c96895f3
Muhammad Jasim <95685947+MJasimTheKnights@users.noreply.github.com>
2021-12-08 15:22:25 +0500

    from bilal jamil

--- app/src/main/java/com/example/stickerapiworking/MainActivity.java
+++ app/src/main/java/com/example/stickerapiworking/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "This is my 5nd repository", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "This is from bilalJamil", Toast.LENGTH_SHORT).show();
     }
 }
